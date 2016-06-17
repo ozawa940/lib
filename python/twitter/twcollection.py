72452fc022fd55e39a0dba3f108e506434dd0bbf
ozawa <d.ozawa040@gmail.com>
2016-06-17 21:15:08 +0900

    Rename from TwitterCollecction.py to twcollection.py

--- python/twitter/twcollection.py
+++ python/twitter/twcollection.py
@@ -21,24 +21,23 @@ Example)
 
 """
 import logging, traceback
-from abc import ABCMeta, abstractmethod
 from twitter import *
 
 # ADT Class
-class TwitterCollection(metaclass=ABCMeta):
+class TwCollectionBase(object):
 
     def __init__(self, keylist):
-        # checker return args not existing dic
+        # _checker return args not existing dic
         _checker = lambda dic, *args: [index for index in args if not index in dic]
-        _not_enough_arg = _checker(keylist, 
+        _not_enough_args = _checker(keylist, 
                         "CONSUMER_KEY", 
                         "CONSUMER_SECRET", 
                         "ACCESS_TOKEN", 
                         "ACCESS_TOKEN_SECRET")
 
-        if len(_not_enough_arg) != 0:
+        if len(_not_enough_args) != 0:
             logging.error(traceback.format_exc())
-            raise Exception(_not_enough_arg + "don't exist in keylist")
+            raise Exception(_not_enough_args + "don't exist in keylist")
         # Inserting keylist
         self.CONSUMER_KEY    = keylist["CONSUMER_KEY"]
         self.CONSUMER_SECRET = keylist["CONSUMER_SECRET"]
@@ -51,42 +50,41 @@ class TwitterCollection(metaclass=ABCMeta):
         except:
             logging.error(traceback.format_exc())
             raise
-
+    # TODO
     # This method return Oauth
     # Version parameter means "1 = user-auth, 2 = app-auth, 3 = stream-auth"
-    def _managingOauth(self, version):
-
-
-
-
-
-
-
-    # This method return bool.
-    # If your account use up rate limit, calling sleep method.
-    def _managingRateLimit(self):
+    def _managing_oauth(self, version):
         pass
 
+
+
+
+
+    # TODO
+    # This method return bool.
+    # If your account use up rate limit, calling sleep method.
+    def _managing_rateLimit(self):
+        pass
 
 
 
 
 
 # Using Twitter REST API
-class TwitterREST(TwitterCollection):
+class TwitterREST(TwCollectionBase):
     # This method return tweet of targeting screen_name until 3200 tweets.
-    def getAccountTweet(self, screen_name):
+    def get_account_tweet(self, screen_name):
         pass
 
 
 # Using Twitter Stream API
-class TwitterStream(TwitterCollection):
+class TwitterStream(TwCollectionBase):
 
 
     # This method is collecting account list from TwitterStream API,
     # and range is whole Japan.
     # Duplicate of account is deleted.
-    def accountList(self, count):
+    def account_list(self, count):
         pass
 
 
